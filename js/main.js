e1c1acdafc7263aa6070bed90da12d7b3174bb85
Julio Melo <juliovilar93@gmail.com>
2016-04-12 04:10:52 -0300

    todas as media queries finalizadas

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $(".btn-mobile").click(function(){
         $('.menu').toggle(200);
     });
+    $('.scroll').click(function(event){
+        event.preventDefault();
+       	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1400);
+   	});
+
 });
